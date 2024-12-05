2728a52b5c1cacb0d15e55e49d2a39911d85b39a
BlendinAMKura <blendi.gr@hotmail.fi>
2024-12-05 13:58:06 +0200

    Fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,67 +1,67 @@
 ////// -------- Map ------------- 
 
-  const map = L.map('map').setView([20, 0], 2);
+const map = L.map('map').setView([20, 0], 2);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 5
-  }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 5
+}).addTo(map);
 
 ////// -------- Countries --------
 
-  const geoJsonUrl = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json';
+const geoJsonUrl = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json';
 
-  const defaultStyle = {
-      color: '#3388ff',
-      weight: 1,
-      fillOpacity: 0.2
-  };
+const defaultStyle = {
+    color: '#3388ff',
+    weight: 1,
+    fillOpacity: 0.2
+};
 
-  const highlightStyle = {
-      color: '#ff0000',
-      weight: 2,
-      fillOpacity: 0.6
-  };
+const highlightStyle = {
+    color: '#ff0000',
+    weight: 2,
+    fillOpacity: 0.6
+};
 
-  const correctStyle = {
-      color: '#00ff00',
-      weight: 2,
-      fillOpacity: 0.6
-  };
+const correctStyle = {
+    color: '#00ff00',
+    weight: 2,
+    fillOpacity: 0.6
+};
 
-  function resetStyle(e) {
-      geojsonLayer.resetStyle(e.target);
-  }
+function resetStyle(e) {
+    geojsonLayer.resetStyle(e.target);
+}
 
 ////// --------- Handle Map Guess ------------
 
-  let geojsonLayer = L.geoJson(null, {
-      style: defaultStyle,
-      onEachFeature: function (feature, layer) {
-          layer.on({
-              mouseover: function (e) {
-                  const layer = e.target;
-                  layer.setStyle(highlightStyle);
-              },
-              mouseout: resetStyle,
+let geojsonLayer = L.geoJson(null, {
+    style: defaultStyle,
+    onEachFeature: function (feature, layer) {
+        layer.on({
+            mouseover: function (e) {
+                const layer = e.target;
+                layer.setStyle(highlightStyle);
+            },
+            mouseout: resetStyle,
 
-              ////// Handle The Click Of The Map -----------
+            ////// Handle The Click Of The Map -----------
 
-              click: function (e) {
-                  const countryName = feature.properties.name;
-                  const resultDiv = document.getElementById('result');
-                  if (countryName === correctCountry) {
-                      resultDiv.innerHTML = `<p>Correct! The photo was taken in ${countryName}.</p>`;
-                      layer.setStyle(correctStyle);
-                  } else {
-                      resultDiv.innerHTML = `<p>Wrong! You selected ${countryName}. Try again.</p>`;
-                  }
-              }
-          });
-      }
-  });
-  fetch(geoJsonUrl)
-      .then(response => response.json())
-      .then(data => geojsonLayer.addData(data).addTo(map));
+            click: function (e) {
+                const countryName = feature.properties.name;
+                const resultDiv = document.getElementById('result');
+                if (countryName === correctCountry) {
+                    resultDiv.innerHTML = `<p>Correct! The photo was taken in ${countryName}.</p>`;
+                    layer.setStyle(correctStyle);
+                } else {
+                    resultDiv.innerHTML = `<p>Wrong! You selected ${countryName}. Try again.</p>`;
+                }
+            }
+        });
+    }
+});
+fetch(geoJsonUrl)
+    .then(response => response.json())
+    .then(data => geojsonLayer.addData(data).addTo(map));
 
 ////// --------- SLIDEBAR ------------
 
@@ -69,5 +69,5 @@ const slider = document.getElementById('yearSlider');
 const display = document.getElementById('yearDisplay');
 
 slider.oninput = function() {
-    display.textContent = this.value;
-  }
+  display.textContent = this.value;
+}
