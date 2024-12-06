e340ba0a40116c49b100295ff33c5df4976c55dd
Blendi Grajqevci <blendi.gr@hotmail.fi>
2024-12-07 00:02:18 +0200

    Image Zoom

--- public/js/game.js
+++ public/js/game.js
@@ -166,4 +166,36 @@ async function getTheRandomLocation() {
       console.log(error);
     });
 }
-getTheRandomLocation();
+getTheRandomLocation()
+
+/// Image Zoom
+
+let currentZoom = 1;
+let minZoom = 1;
+let maxZoom = 3;
+let stepSize = 0.1;
+
+let container = document.getElementById("photo-container");
+
+container.addEventListener("wheel", function(event) {
+    // Zoom in or out based on the scroll direction
+    let direction = event.deltaY > 0 ? -1 : 1;
+    zoomImage(direction);
+});
+
+function zoomImage(direction)
+{
+    let newZoom = currentZoom + direction * stepSize;
+
+    // Limit the zoom level to the minimum and maximum
+    // values
+    if (newZoom < minZoom || newZoom > maxZoom) {
+        return;
+    }
+
+    currentZoom = newZoom;
+
+    // Update the CSS transform of the image to scale it
+    let image = document.querySelector("#photo");
+    image.style.transform = "scale(" + currentZoom + ")";
+}
