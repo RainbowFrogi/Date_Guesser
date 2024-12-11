8ba72fab38131d6346d83e0085b6296da37bdaac
Juan-Valen <juan.valenz.101@hotmail.com>
2024-12-11 23:27:54 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -10,16 +10,13 @@ from random import randint
 # Server
 app = Flask(__name__, template_folder='templates')#, static_folder='public')
 # ---------------------
-#   FUNCTIONS
-# ---------------------
-# ---------------------
 #   ROUTES
 # ---------------------
 # STATIC ---------------------
 @app.route('/public/<path:filename>')
 def public(filename):
-    return send_from_directory('public', filename, as_attachment=True) # SERVER FILE FROM FOLDER 'PUBLIC'       # app.static_folder
-    # TRY localhost:3000/public/css/style.css to understand how this route works.
+    return send_from_directory('public', filename) # SERVER FILE FROM FOLDER 'PUBLIC'       # app.static_folder
+    # TRY localhost:3000/public/css/home.css to understand how this route works.
 
 # DYNAMIC ---------------------
 @app.route("/")
@@ -36,14 +33,6 @@ def game():
 def result(result: str):
     # The following function is considered a templating engine/processor/parser
     return render_template("result.html", result=result)     #   It returns the "result.html" file from the "templates" folder
-
-# @app.route("/api/guess/<id>/<year>/<country>")
-# def result(id: int, year: int, country: str):
-# print(result)
-#   # The following function is considered a templating engine/processor/parser
-#     return render_template("result.html", result=result)
-#     #   It returns the "result.html" file from the "templates" folder
-#     #   and sets the result variable as the value from the path
 
 @app.route("/api/game/location/random/")
 def api_game_location_random():
@@ -63,7 +52,6 @@ def api_game_guess():
     body = request.get_json()
     answer = is_location_correct(body["id"], body["year"], body["country"])
     return answer
-
 
 @app.errorhandler(404)
 def page_not_found(error):
